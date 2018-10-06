a66903a437e73e22a54443200a662724143e1e24
Daniel Nolan <DanNolan99@gmail.com>
2018-10-06 01:36:17 -0700

    basic routing plus redux

---
+++ client/src/components/Trading.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Header from './Header';
+
+class Trading extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Header />
+        <div className="trading">
+          Trading
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Trading;
